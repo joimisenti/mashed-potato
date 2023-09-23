52c69e4703e912326629ae3eac25a92d8a7c6830
Joi Misenti <jmisenti@Jois-MacBook-Air.local>
2023-09-23 19:25:02 -0300

    added controller endpoints

--- src/main/java/com/bpCapstone/daybreakBackup/controllers/LoadoutController.java
+++ src/main/java/com/bpCapstone/daybreakBackup/controllers/LoadoutController.java
@@ -1,13 +1,40 @@
 package com.bpCapstone.daybreakBackup.controllers;
 
+import com.bpCapstone.daybreakBackup.dtos.LoadoutDto;
 import com.bpCapstone.daybreakBackup.services.LoadoutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/loadouts")
 public class LoadoutController {
     @Autowired
     private LoadoutService loadoutService;
+
+    @GetMapping("/user/{userId}")
+    public List<LoadoutDto> getLoadoutsByUser(Long userId) {
+        return loadoutService.getAllLoadoutsByUserId(userId);
+    }
+
+    @PostMapping("/user/{userId}")
+    public void addLoadout(@RequestBody LoadoutDto loadoutDto,@PathVariable Long userId){
+        loadoutService.addLoadout(loadoutDto, userId);
+    }
+
+    @DeleteMapping("/{loadoutId}")
+    public void deleteLoadoutById(@PathVariable Long loadoutId) {
+        loadoutService.deleteLoadoutById(loadoutId);
+    }
+    @PutMapping
+    public void updateLoadout(@RequestBody LoadoutDto loadoutDto) {
+        loadoutService.updateLoadoutById((loadoutDto));
+    }
+
+    @GetMapping("/{loadoutId}")
+    public Optional<LoadoutDto> getLoadoutById(@PathVariable Long loadoutId) {
+        return loadoutService.getLoadoutById(loadoutId);
+    }
 }
--- src/main/java/com/bpCapstone/daybreakBackup/controllers/PerkController.java
+++ src/main/java/com/bpCapstone/daybreakBackup/controllers/PerkController.java
@@ -1,13 +1,37 @@
 package com.bpCapstone.daybreakBackup.controllers;
 
+import com.bpCapstone.daybreakBackup.dtos.PerkDto;
+import com.bpCapstone.daybreakBackup.entities.Perk;
 import com.bpCapstone.daybreakBackup.services.PerkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/perks")
 public class PerkController {
     @Autowired
     private PerkService perkService;
+
+    @GetMapping("/perks")
+    public List<Perk> getAllPerks() {
+        return perkService.getAllPerks();
+    }
+
+    @DeleteMapping("/{perkId}")
+    public void deletePerkById(@PathVariable Long perkId) {
+        perkService.deletePerkById(perkId);
+    }
+
+    @PutMapping
+    public void updatePerk(@RequestBody PerkDto perkDto) {
+        perkService.updatePerkById(perkDto);
+    }
+
+    @GetMapping("/{perkId}")
+    public Optional<PerkDto> getPerkById(@PathVariable Long perkId) {
+        return perkService.getPerkById(perkId);
+    }
 }
